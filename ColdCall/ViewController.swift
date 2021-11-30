d1f30c45b2e4b044ae6c32741f784e76ea1850c6
Maha <Memo1992mm2010@hotmail.com>
2021-11-30 12:19:32 +0300

    ......

--- ColdCall/ViewController.swift
+++ ColdCall/ViewController.swift
@@ -10,12 +10,29 @@ import UIKit
 class ViewController: UIViewController {
     
     var names : [String] = ["Maha","Noha","Hala","Maya","Farah"]
+ 
+    
+    @IBOutlet weak var numberchang: UILabel!
     
     @IBOutlet weak var namelist: UILabel!
     
     @IBAction func buttonpressed(_ sender: UIButton) {
-        namelist.text=names[Int.random(in: 0..<5)]
-    }
+        
+        namelist.text=names.randomElement()!
+        
+        let num = Int.random(in: 1...5)
+        
+        numberchang.text = String(num)
+        
+            if num == 1 || num == 2 {
+                numberchang.textColor = UIColor.systemRed
+            }else if num == 3 || num == 4 {
+                numberchang.textColor = UIColor.systemOrange
+            }else if num == 5 {
+                numberchang.textColor = UIColor.systemGreen
+            }
+        }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
